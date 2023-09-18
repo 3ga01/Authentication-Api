b05cccb09c5bc93b62b09e83134684d4d04a756c
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-09-18 19:45:28 +0000

    Project Created

---
+++ src/test/java/com/example/AuthenticationApi/AuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthenticationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
